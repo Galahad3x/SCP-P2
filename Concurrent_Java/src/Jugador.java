8d521851bb5cddf8208e83cc3d6e001cb3811fc9
Joe <joelfarrecortes@gmail.com>
2020-11-06 17:26:24 +0100

    Crear classe Jugador amb Java

---
+++ Concurrent_Java/src/Jugador.java
@@ -0,0 +1,14 @@
+enum TJugador {Porter, Defensa, Migcampista, Delanter}
+
+public class Jugador {
+    public String nom;
+    public int preu;
+    public int valor;
+    public TJugador posicio;
+    public Jugador (String nom, int preu, int valor, TJugador posicio) {
+        this.nom = nom;
+        this.preu = preu;
+        this.valor = valor;
+        this.posicio = posicio;
+    }
+}
