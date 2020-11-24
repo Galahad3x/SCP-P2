259f72e6edd6f9ed1ffcc9305c5c6fc2d0c9c980
Joe <joelfarrecortes@gmail.com>
2020-11-24 15:25:16 +0100

    Casi casi

--- Concurrent_Java/src/Manfutc.java
+++ Concurrent_Java/src/Manfutc.java
@@ -10,12 +10,22 @@ public class Manfutc {
     public static JugadorsEquip agafar = new JugadorsEquip();
     public static Equip agafar_equip = new Equip(0, 0, 0, 0, agafar);
 
-    public static void main(String[] argvs) {
+    public static ManfutcThreads[] threads_arr;
+    public static boolean[] threads_act;
+
+    public static final Object lock = new Object();
+
+    public static Equip[] thread_return;
+
+    public static void main(String[] argvs) throws InterruptedException {
         if (argvs.length != 3) {
             throw new IllegalArgumentException("Error while introduce the arguments: <pressupost>, <nom_mercat>, <n_threads>.");
         }
         int pressupost = Integer.parseInt(argvs[0]);
         n_threads = Integer.parseInt(argvs[2]);
+        threads_arr = new ManfutcThreads[n_threads];
+        threads_act = new boolean[n_threads];
+        thread_return = new Equip[n_threads];
 
         Mercat mercat = new Mercat();
         JugadorsEquip jugadorsEquip = new JugadorsEquip();
@@ -98,7 +108,7 @@ public class Manfutc {
     }
 
     // Calculates the best team
-    public static Equip calcularEquipOptim(Mercat mercat, Equip equip, int index) {
+    public static Equip calcularEquipOptim(Mercat mercat, Equip equip, int index) throws InterruptedException {
         if (index == 0) {
             if (equip.playerFits(mercat.getJugador(index)) && equip.isRepeated(mercat.getJugador(index))) {
                 equip.jugadorsEquip.addPlayer(mercat.getJugador(index));
@@ -106,27 +116,55 @@ public class Manfutc {
                 id_equips++;
             }
         } else {
+
+            int val_agafar = 0;
+
+            int t_index = -1;
+
+            // In the case we pick the player
+            if (equip.playerFits(mercat.getJugador(index)) && equip.isRepeated(mercat.getJugador(index))) {
+                synchronized (lock) {
+                    for (int i = 0; i < n_threads; i++) {
+                        if (!threads_act[i]) {
+                            t_index = i;
+                            break;
+                        }
+                    }
+                    if (t_index != -1) {
+                        threads_act[t_index] = true;
+                    }
+                }
+                if (t_index != -1) {
+                    threads_arr[t_index] = new ManfutcThreads(mercat, equip, index, t_index);
+                    threads_arr[t_index].start();
+                } else {
+                    agafar_equip.id = equip.id;
+                    agafar_equip.valor = equip.valor + (mercat.getJugador(index).valor);
+                    agafar_equip.cost = equip.cost + (mercat.getJugador(index).preu);
+                    agafar_equip.pressupost = equip.pressupost - (mercat.getJugador(index).preu);
+                    agafar_equip.jugadorsEquip = equip.jugadorsEquip;
+                    agafar_equip.jugadorsEquip.addPlayer(mercat.getJugador(index));
+                    agafar_equip = calcularEquipOptim(mercat, agafar_equip, index - 1);
+                    val_agafar = agafar_equip.valor;
+                }
+            }
+
             // In the case we don't pick the player
             no_agafar_equip.id = equip.id;
             no_agafar_equip.valor = equip.valor;
             no_agafar_equip.cost = equip.cost;
             no_agafar_equip.pressupost = equip.pressupost;
             no_agafar_equip.jugadorsEquip = equip.jugadorsEquip;
-            calcularEquipOptim(mercat, no_agafar_equip, index - 1);
+            no_agafar_equip = calcularEquipOptim(mercat, no_agafar_equip, index - 1);
 
-            // In the case we pick the player
-            if (equip.playerFits(mercat.getJugador(index)) && equip.isRepeated(mercat.getJugador(index))) {
-                agafar_equip.id = equip.id;
-                agafar_equip.valor = equip.valor + (mercat.getJugador(index).valor);
-                agafar_equip.cost = equip.cost + (mercat.getJugador(index).preu);
-                agafar_equip.pressupost = equip.pressupost - (mercat.getJugador(index).preu);
-                agafar_equip.jugadorsEquip = equip.jugadorsEquip;
-                agafar_equip.jugadorsEquip.addPlayer(mercat.getJugador(index));
-                calcularEquipOptim(mercat, agafar_equip, index - 1);
+            if (t_index != -1) {
+                threads_arr[t_index].join();
+                agafar_equip = thread_return[t_index];
+                threads_act[t_index] = false;
             }
 
             // We check which equip fits better
-            if (agafar_equip.valor == 0 || no_agafar_equip.valor > agafar_equip.valor) {
+            if (val_agafar == 0 || no_agafar_equip.valor > agafar_equip.valor) {
                 equip.id = no_agafar_equip.id;
                 equip.valor = no_agafar_equip.valor;
                 equip.cost = no_agafar_equip.cost;
@@ -144,15 +182,32 @@ public class Manfutc {
     }
 
     public static class ManfutcThreads extends Thread{
+        public Mercat mercat;
+        public Equip equip;
         public int index;
+        public int t_index;
 
-        public ManfutcThreads(int index) {
+        public ManfutcThreads(Mercat mercat, Equip equip, int index, int t_index) {
+            this.mercat = mercat;
+            this.equip = equip;
             this.index = index;
+            this.t_index = t_index;
         }
 
         @Override
         public void run() {
-
+            agafar_equip.id = equip.id;
+            agafar_equip.valor = equip.valor + (mercat.getJugador(index).valor);
+            agafar_equip.cost = equip.cost + (mercat.getJugador(index).preu);
+            agafar_equip.pressupost = equip.pressupost - (mercat.getJugador(index).preu);
+            agafar_equip.jugadorsEquip = equip.jugadorsEquip;
+            agafar_equip.jugadorsEquip.addPlayer(mercat.getJugador(index));
+            try {
+                agafar_equip = calcularEquipOptim(mercat, agafar_equip, index - 1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            thread_return[t_index] = agafar_equip;
         }
     }
 }
